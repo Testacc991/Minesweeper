cff3c65ecd08ee58e725863be5ed5027aae01af2
Testacc991 <70212365+Testacc991@users.noreply.github.com>
2022-07-15 18:03:18 +0300

    added background

--- main/data.lua
+++ main/data.lua
@@ -3,15 +3,15 @@ M.hardness = 10
 M.mult = 0.5
 M.columns = 0
 M.rows = 0
-M.tilesize = math.floor(64*M.mult)
+M.tilesize = math.floor(66*M.mult)
 M.scale = 1*M.mult
 
-function M.world2tile(p)
-	return vmath.vector3(math.floor((p.x+M.tilesize)/M.tilesize),
-	math.floor((p.y+M.tilesize)/M.tilesize),0)
-end
-function M.tile2world(p)
-	return vmath.vector3((p.x * M.tilesize)-(M.tilesize/2),
-	(p.y * M.tilesize)-(M.tilesize/2),0)
-end
+--function M.world2tile(p)
+--return vmath.vector3(math.floor((p.x+M.tilesize)/M.tilesize),
+--	math.floor((p.y+M.tilesize)/M.tilesize),0)
+--end
+--function M.tile2world(p)
+--	return vmath.vector3((p.x * M.tilesize)-(M.tilesize/2),
+--	(p.y * M.tilesize)-(M.tilesize/2),0)
+--end
 return M
